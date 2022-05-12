1be6fd462a461cec8d99f4778526a7fdb717a1ac
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-05-12 15:09:44 +0200

    Scopes

--- JavaScript/01-hello.js
+++ JavaScript/01-hello.js
@@ -7,6 +7,14 @@ function hello(name) {
   return 'Hello ' + name;
 }
 
+// const hello = function (name) {
+//   return 'Hello ' + name;
+// };
+
+// setTimeout(function (name) {
+//   return 'Hello ' + name;
+// }, 1000);
+
 for (const n of names) {
   console.log(hello(n));
 }
---
+++ JavaScript/03-scopes.js
@@ -0,0 +1,29 @@
+globalThis.maVarGlobal = 'maVarGlobal';
+const maVarScript = 'maVarScript'; // script / module (ça dépend)
+
+function externe() {
+  const maVarClosure = 'maVarClosure';
+
+  function interne() {
+    const maVarLocal = 'maVarLocal';
+
+    if (true) {
+      const maVarBlock = 'maVarBlock';
+      console.log('maVarBlock', maVarBlock);
+      console.log('maVarLocal', maVarLocal);
+      console.log('maVarClosure', maVarClosure);
+      console.log('maVarScript', maVarScript);
+      console.log('maVarGlobal', maVarGlobal);
+    }
+
+    // console.log('en dehors du if', typeof maVarBlock);
+  }
+
+  interne();
+
+}
+
+externe()
+
+console.log(typeof maVar); // undefined
+console.log(typeof interne); // undefined
