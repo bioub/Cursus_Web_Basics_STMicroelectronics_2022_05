7242a067328a6281b941702f4e51279cc435a840
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-05-12 12:58:09 +0200

    JIT

---
+++ JavaScript/02-compilation-jit.js
@@ -0,0 +1,24 @@
+function hello(name) {
+  return 'Hello ' + name;
+}
+
+console.time('hello');
+hello('Romain');
+console.timeEnd('hello');
+
+console.time('hello');
+hello('Eric');
+console.timeEnd('hello');
+
+console.time('hello');
+hello('Martin');
+console.timeEnd('hello');
+
+console.time('hello');
+hello(123);
+console.timeEnd('hello');
+
+console.time('hello');
+hello('Philippe');
+console.timeEnd('hello');
+
