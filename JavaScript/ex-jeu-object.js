082ad03399b413ec0f6dce434ec9825be1c7414d
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-05-13 14:41:35 +0200

    Jeu Object

---
+++ JavaScript/ex-jeu-object.js
@@ -0,0 +1,65 @@
+// Priviléger 4 fonction à un objet qui regroupe des méthodes
+// (mieux pour le Tree Shaking)
+const Random = {
+  get: function () {
+    return Math.random();
+  },
+  getArbitrary: function (min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getInt: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  },
+  getIntInclusive: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  },
+};
+
+const readline = require('readline');
+
+function Jeu(options) {
+  options = options || {};
+  const min = options.min || 0;
+  const max = options.max !== undefined ? options.max : 100;
+  this.entierAlea = Random.getInt(min, max);
+  this.essais = [];
+  this.rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+}
+
+Jeu.prototype.jouer = function() {
+  if (this.essais.length) {
+    console.log('Vous avez déjà joué : ' + this.essais.join(' - '));
+  }
+
+  this.rl.question('Quel est le nombre ? ', (answer) => {
+    const entierSaisi = parseInt(answer, 10);
+
+    if (isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un entier');
+      return this.jouer();
+    }
+
+    this.essais.push(entierSaisi);
+
+    if (entierSaisi < this.entierAlea) {
+      console.log('Trop petit');
+      this.jouer();
+    } else if (entierSaisi > this.entierAlea) {
+      console.log('Trop grand');
+      this.jouer();
+    } else {
+      console.log('Gagné');
+      this.rl.close();
+    }
+  });
+}
+
+const game = new Jeu();
+game.jouer();
